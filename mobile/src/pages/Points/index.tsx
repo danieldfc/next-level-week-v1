fc40f6bcf6ea570d0b3666dc9bc323c74e3a96db
Daniel Felizardo <daniel.david772@gmail.com>
2020-06-05 18:05:40 -0300

    feat: validations and create of files

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -27,6 +27,7 @@ interface Point {
   id: number;
   name: string;
   image: string;
+  image_url: string;
   latitude: number;
   longitude: number;
 }
@@ -125,6 +126,7 @@ const Points: React.FC = () => {
               >
               {points.map(point => (
                 <Marker
+                  key={point.id}
                   style={styles.mapMarker}
                   onPress={() => handleNavigateToDetail(point.id)}
                   coordinate={{
@@ -133,7 +135,7 @@ const Points: React.FC = () => {
                   }}
                 >
                   <View style={styles.mapMarkerContainer}>
-                    <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
+                    <Image style={styles.mapMarkerImage} source={{ uri: point.image_url }} />
                     <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                   </View>
                 </Marker>
--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,14 @@ export default class PointsController {
       .distinct()
       .select('points.*');
 
-    return response.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.36.143:3333/uploads/${point.image}`
+      }
+    });
+
+    return response.json(serializedPoints);
   }
   
   async show(request: Request, response: Response): Promise<Response> {
@@ -29,12 +36,18 @@ export default class PointsController {
       return response.status(400).json({ message: 'Point not found' })
     }
 
+    const serializedPoint = {
+        ...point,
+        image_url: `http://192.168.36.143:3333/uploads/${point.image}`
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+
+    return response.json({ point: serializedPoint, items });
   }
 
   async create(request: Request, response: Response): Promise<Response> {
@@ -52,7 +65,7 @@ export default class PointsController {
     const trx = await knex.transaction();
   
     const point = {
-      image: 'image-fake',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -66,12 +79,14 @@ export default class PointsController {
   
     const point_id = insertedIds[0]
   
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      }
-    })
+    const pointItems = items.split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        }
+      })
   
     await trx('point_items').insert(pointItems);
   
